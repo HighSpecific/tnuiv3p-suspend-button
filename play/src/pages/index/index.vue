385badb61f6f40afeb42910742033c769586d93a
jaylen <350683609@qq.com>
2023-11-06 15:27:59 +0800

    初始版本发布

---
+++ play/src/pages/index/index.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+import TnSuspendButton from '../../packages/src/index.vue'
+
+const message = ref<string>('欢迎开发TuniaoUI v3 uniapp 插件')
+</script>
+
+<template>
+  <view class="content">
+    <view class="message">{{ message }}</view>
+    <TnSuspendButton icon="logo-tuniao" shape="circle" />
+  </view>
+</template>
+
+<style lang="scss" scoped>
+.content {
+  position: relative;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+
+  .message {
+    font-size: 50rpx;
+  }
+}
+</style>
